dc7d0c7f6299cc905f894cf2a9486ff76abb77c0
hanyufan <1041218477@qq.com>
2019-12-18 22:55:06 +0800

    黑马头条PC-初始化项目-页面分析-新建登录和主页

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@ import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css' // 引入样式
-
-Vue.ues(ElementUI)
+import './styles/index.less' // 引入初始化样式
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
