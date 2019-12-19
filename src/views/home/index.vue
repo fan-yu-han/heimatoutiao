f630cc07df5c9815c57371d01e11d6f168b951a3
hanyufan <1041218477@qq.com>
2019-12-19 22:28:34 +0800

    黑马头条PC-主页模块-页面布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,18 @@
 <template>
-  <div>主页</div>
+<!-- 一个大容器 -->
+  <el-container>
+    <!-- 分左右布局 -->
+    <el-aside>
+      左侧内容
+    </el-aside>
+    <!-- 右侧内容 -->
+    <el-container>
+      <!-- 右侧里面再分上下 上面是导航 下面是主要容器 -->
+      <el-header>头部</el-header>
+      <el-main>主要区域</el-main>
+    </el-container>
+  </el-container>
+
 </template>
 
 <script>
