0299150a133d9a624bb7a2c44294bed93048c8b7
hanyufan <1041218477@qq.com>
2019-12-19 11:28:03 +0800

    黑马头条PC-登录模块-数据绑定及校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -5,22 +5,22 @@
       <div class="title">
         <img src="../../assets/img/logo_index.png" alt="">
       </div>
-       <el-form>
+       <el-form ref="myForm" :model='loginForm' :rules="loginRules">
         <!-- 表单域 里面   放置 input/select/checkbox 相当于一行-->
-        <el-form-item>
-           <el-input placeholder="请输入手机号"></el-input>
+        <el-form-item prop="mobile">
+           <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
         </el-form-item>
         <!-- 表单域 -->
-        <el-form-item>
-          <el-input style="width:65%" placeholder="验证码"></el-input>
-            <el-button style="float:right" plain>发送验证码</el-button>
+        <el-form-item prop="code">
+          <el-input v-model="loginForm.code" style="width:65%" placeholder="验证码"></el-input>
+            <el-button  style="float:right" plain>发送验证码</el-button>
         </el-form-item>
-        <el-form-item>
+        <el-form-item prop="check">
           <!-- 复选框 -->
-          <el-checkbox>我已阅读并同意用户协议和隐私条款</el-checkbox>
+          <el-checkbox v-model="loginForm.check">我已阅读并同意用户协议和隐私条款</el-checkbox>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" style="width:100%">登录</el-button>
+          <el-button @click="submitLogin" type="primary" style="width:100%">登录</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -29,9 +29,43 @@
 </template>
 
 <script>
+// import { log } from 'util'
 export default {
-
+  data () {
+    return {
+      loginForm: {
+        mobile: '',
+        code: '',
+        check: false
+      },
+      loginRules: {
+        mobile: [{ required: true, message: '清输入你的手机号' }, {
+          pattern: /^1[3456789]\d{9}$/, message: '请输入合法的手机号'
+        }],
+        code: [{ required: true, message: '清输入你密码' },
+          { pattern: /^\d{6}$/, message: '验证码为6位数字' }],
+        check: [{ validator: function (rule, value, callback) {
+          if (value) {
+            callback()
+          } else {
+            callback(new Error('你应该无条件选择'))
+          }
+        } }]
+      }
+    }
+  },
+  methods: {
+    submitLogin () {
+      this.$refs.myForm.validate(function (isOK) {
+        if (isOK) {
+          // 说明通过
+          console.log('校验通过')
+        }
+      })
+    }
+  }
 }
+
 </script>
 
 <style lang='less' scoped>
