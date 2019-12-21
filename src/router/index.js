d3ba049d32f011edb1c407ecca9746d8bf7dc845
hanyufan <1041218477@qq.com>
2019-12-21 11:32:42 +0800

    黑马头条PC-主页模块-默认导航

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/home'
 import Login from '../views/login'
-
+import Home2 from '../views/home/heme.vue'
 Vue.use(VueRouter)
 
 const routes = [
@@ -13,7 +13,11 @@ const routes = [
   {
     path: '/home',
     name: 'home',
-    component: Home
+    component: Home,
+    children: [{
+      path: '',
+      component: Home2
+    }]
   }, {
     path: '/login',
     component: Login
---
+++ src/views/home/heme.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <el-carousel :interval="4000" type="card" height="300px">
+        <el-carousel-item v-for="item in list" :key="item">
+        <img :src="item" alt="">
+        </el-carousel-item>
+         </el-carousel>
+    </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      list: [
+        'http://img2.imgtn.bdimg.com/it/u=248395548,980320538&fm=26&gp=0.jpg',
+        'http://img2.imgtn.bdimg.com/it/u=1299969344,1554668160&fm=26&gp=0.jpg',
+        'http://img4.imgtn.bdimg.com/it/u=1034988724,142912014&fm=26&gp=0.jpg']
+    }
+  }
+}
+</script>
+
+<style>
+.el-carousel__item h3 {
+    color: #475669;
+    font-size: 14px;
+    opacity: 0.75;
+    line-height: 200px;
+    margin: 0;
+  }
+
+  .el-carousel__item:nth-child(2n) {
+    background-color: #99a9bf;
+  }
+
+  .el-carousel__item:nth-child(2n+1) {
+    background-color: #d3dce6;
+  }
+</style>
