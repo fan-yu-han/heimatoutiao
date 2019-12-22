ea9880037a3f585ec0d3631b831c2647fc62e40d
hanyufan <1041218477@qq.com>
2019-12-22 11:25:23 +0800

    评论列表-页面结构-table

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -1,7 +1,20 @@
 <template>
-<div>
-    评论列表
-</div>
+    <el-card>
+        <bread-crumb slot="header">
+        <!-- 插槽内容 -->
+            <template slot="title">
+                评论管理
+            </template>
+        </bread-crumb>
+        <el-table>
+            <el-table-column width="600" label="标题"></el-table-column>
+            <el-table-column label="评论状态"></el-table-column>
+            <el-table-column label="总评论数"></el-table-column>
+            <el-table-column label="粉丝评论数"></el-table-column>
+            <el-table-column label="操作"></el-table-column>
+        </el-table>
+    </el-card>
+
 </template>
 
 <script>
