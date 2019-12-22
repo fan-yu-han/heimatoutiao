7ba2e9425eb8c09aa364729c7c9226466bfa1a80
hanyufan <1041218477@qq.com>
2019-12-22 21:46:42 +0800

    评论列表-评论数据加载到页面及状态过滤

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -6,12 +6,20 @@
                 评论管理
             </template>
         </bread-crumb>
-        <el-table>
-            <el-table-column width="600" label="标题"></el-table-column>
-            <el-table-column label="评论状态"></el-table-column>
-            <el-table-column label="总评论数"></el-table-column>
-            <el-table-column label="粉丝评论数"></el-table-column>
-            <el-table-column label="操作"></el-table-column>
+        <el-table :data='list '>
+            <!-- prop 字段名   -->
+            <el-table-column prop="title" width="500" label="标题"></el-table-column>
+            <el-table-column  :formatter="formatterBoolean"  prop="comment_status" label="评论状态"></el-table-column>
+            <el-table-column prop="total_comment_count" label="总评论数"></el-table-column>
+            <el-table-column prop="fans_comment_count" label="粉丝评论数"></el-table-column>
+            <el-table-column label="操作">
+                <template slot-scope="obj">
+                <el-button size='small' type="text">修改</el-button>
+                <!-- 需要给你举状态进行判断关闭还是打开 -->
+                <el-button size='small' type="text">{{obj.row.comment_status ? '关闭' :'打开'}}评论</el-button>
+
+                </template>
+            </el-table-column>
         </el-table>
     </el-card>
 
@@ -19,7 +27,27 @@
 
 <script>
 export default {
-
+  data () {
+    return {
+      list: []
+    }
+  },
+  methods: {
+    getComment () {
+      this.$axios({
+        url: '/articles',
+        params: { response_type: 'comment' }
+      }).then(result => {
+        this.list = result.data.results
+      })
+    },
+    formatterBoolean (row, column, cellValue, index) {
+      return cellValue ? '正常' : '关闭'
+    }
+  },
+  created () {
+    this.getComment()
+  }
 }
 </script>
 
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,7 +2,7 @@
 <!-- 一个大容器 -->
   <el-container>
     <!-- 分左右布局 -->
-    <el-aside  style="height: 100vh; background-color: #353b4e;width:230px">
+    <el-aside  style="min-height: 100vh; background-color: #353b4e;width:230px">
      <layout-aside ></layout-aside>
     </el-aside>
     <!-- 右侧内容 -->
