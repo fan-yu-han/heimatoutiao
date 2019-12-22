62da5124fd632e2897ccb46d46d3895b0e012551
hanyufan <1041218477@qq.com>
2019-12-22 09:58:43 +0800

    黑马头条PC--接口访问-axios拦截器-统一处理异常数据

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,8 @@
 // 封装一个axios
 import axios from 'axios'
+// import router from '../router'
+import { Message } from 'element-ui'
+import router from '../router'
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
 // 请求拦截器
 axios.interceptors.request.use(function (config) {
@@ -16,7 +19,29 @@ axios.interceptors.request.use(function (config) {
 axios.interceptors.response.use(function (response) {
 // 成功是执行该函数 状态吗 200/201/204
   return response.data ? response.data : {}
-}, function () {
+}, function (error) {
 // 失败是执行该参数
+  let status = error.response.status
+  let message = ''
+  switch (status) {
+    case 400:
+      message = '请求参数错误'
+      break
+    case 507:
+      message = '服务器错误'
+      break
+    case 401:
+      // token 过期或者失败
+      // this.$router
+      window.localStorage.removeItem('user-token')// 删除过期token
+      router.push('.login')
+      break
+    case 403:
+      message = '没有设置这条评论的权限'
+      break
+    default:
+      break
+  }
+  Message({ type: 'warning', message })// 提示信息
 })
 export default axios
