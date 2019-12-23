4c31d1fa1d9295656a82b882e2ed3caf13db117e
hanyufan <1041218477@qq.com>
2019-12-23 16:16:32 +0800

    素材列表-新建页面-挂载路由

--- src/router/index.js
+++ src/router/index.js
@@ -20,6 +20,9 @@ const routes = [
     }, {
       path: '/home/comment',
       component: () => import('../views/comment')
+    }, {
+      path: '/home/material',
+      component: () => import('../views/material')
     }]
   }, {
     path: '/login',
