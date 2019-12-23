e0c389fd1aedf5738d0aa5ac32976560232670de
hanyufan <1041218477@qq.com>
2019-12-23 16:34:18 +0800

    素材列表-页面结构-tab

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -5,12 +5,21 @@
             素材管理
         </template>
       </bread-crumb>
+      <!-- 标签页 -->
+       <el-tabs v-model="activeName">
+           <el-tab-pane label="全部图片" name="all">全部图片</el-tab-pane>
+           <el-tab-pane label="收藏图片" name="collect">收藏图片</el-tab-pane>
+        </el-tabs>
   </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      activeName: 'all'// 当前选中的标签
+    }
+  }
 }
 </script>
 
