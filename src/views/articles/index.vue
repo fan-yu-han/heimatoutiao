a7d6cc40c8de3d7ab1530f73b5b9c8fb3c2ec53a
hanyufan <1041218477@qq.com>
2019-12-25 09:16:32 +0800

    内容列表-页面结构-搜索工具栏

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -1,15 +1,62 @@
 <template>
   <el-card>
-      <bread-crumb slot="head">
+      <bread-crumb slot="header">
         <template slot="title">文章列表</template>
 
       </bread-crumb>
+      <!-- 表单容器  -->
+      <el-form style="padding-left:50px">
+          <el-form-item label="文章状态:">
+              <!-- 放置一个单选组  文章状态，0-草稿，1-待审核，2-审核通过，3-审核失败，4-已删除，不传为全部-->
+              <el-radio-group v-model="searchForm.status">
+              <!-- label  -->
+                <el-radio :label="5">全部</el-radio>
+                <el-radio :label="0">草稿</el-radio>
+                <el-radio :label="1">待审核</el-radio>
+                <el-radio :label="2">审核通过</el-radio>
+                <el-radio  :label="3">审核失败</el-radio>
+               </el-radio-group>
+               {{searchForm}}
+          </el-form-item>
+          <el-form-item label="频道列表:">
+
+              <el-select placeholder="请选择频道" v-model="searchForm.channel_id ">
+                  <el-option v-for='item in channels' :key='item.id' :label="item.name" :value="item.id" ></el-option>
+              </el-select>
+          </el-form-item>
+          <el-form-item label="时间选择:">
+              <!-- 日期选择器 -->
+              <el-date-picker v-model="searchForm.dateRange" type="daterange"></el-date-picker>
+          </el-form-item>
+      </el-form>
   </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      searchForm: {
+        status: 5, // 默认悬着全部
+        channel_id: null, //
+        dateRange: [ ] // 日期范围
+      },
+      channels: []// 接收频道数据
+    }
+  },
+  methods: {
+    // 获取所有的pindao
+    getChannels () {
+      this.$axios({
+        url: '/channels'
+      }).then(result => {
+        this.channels = result.data.channels
+      })
+    }
+  },
+  created () {
+    this.getChannels()
+  }
 }
 </script>
 
