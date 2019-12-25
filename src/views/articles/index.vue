d3a2e0ba6bd6c2086dfbbc052170863f2cea8fc7
hanyufan <1041218477@qq.com>
2019-12-25 11:18:09 +0800

    $ git commit -m 内容列表-页面主体结构

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-card>
+  <el-card class="articles">
       <bread-crumb slot="header">
         <template slot="title">文章列表</template>
 
@@ -29,6 +29,25 @@
               <el-date-picker v-model="searchForm.dateRange" type="daterange"></el-date-picker>
           </el-form-item>
       </el-form>
+      <el-row class="total" type="flex" align="middle ">
+        <span>共找到10000条符合条件的内容</span>
+      </el-row>
+      <div class="article-item" v-for="item in 100" :key="item">
+        <!-- 左侧  -->
+        <div class="left">
+           <img src="../../assets/img/back.png" alt="">
+           <div class="info">
+              <span>我欲封天</span>
+              <el-tag class="tag">标签</el-tag>
+              <span>2019年12月25日10:26:13</span>
+          </div>
+        </div>
+        <!-- 右侧 -->
+        <div class="right">
+            <span> <i class="el-icon-edit"> </i>修改</span>
+            <span><i class="el-icon-delete"></i>删除</span>
+        </div>
+      </div>
   </el-card>
 </template>
 
@@ -60,6 +79,47 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang='less' scoped>
+  .articles{
+      .total{
+        height: 60px;
+        border-bottom: 1px dashed #ccc  ;
+      }
+      .article-item {
+        display: flex;
+        justify-content: space-between;
+        padding: 20px 0;
+        border-bottom: 1px solid #f2f3f5;
+          .left{
+            display: flex;
+              img{
+                width: 180px;
+                height: 100px;
+                border-radius: 4px
+                }
+              .info {
+                  height: 100px;
+                  margin-left: 10px;
+                  display: flex;
+                  flex-direction:column;
+                  justify-content: space-around;
+                  .date{
+                    color: #999;
+                    font-style: 12px;
+                  }
+                  .tag {
+                    text-align: center;
+                    width: 60px;
+                  }
+                }
+      }
+          .right {
+            span {
+              font-size: 14px;
+              margin-right: 8px;
+              cursor: pointer;
+            }
+          }
+  }
+  }
 </style>
