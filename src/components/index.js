b082554b9bdf71fbdb6d5a6cddb5a5b22b7c87bd
hanyufan <1041218477@qq.com>
2019-12-27 22:43:38 +0800

    注册富文本编辑器并使用

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,17 @@
 import layoutAside from './home/layout-aside'
 import layoutHeader from './home/layout-header'
 import breadCrumb from './common/bread-crumb'
+import { quillEditor } from 'vue-quill-editor'// qull富文本编辑器
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
 export default {
   install (Vue) {
     Vue.component('layout-aside', layoutAside)
     Vue.component('layout-header', layoutHeader)// 注册一个全局主见
     Vue.component('bread-crumb', breadCrumb) // 全局注册面包屑组件
+    Vue.component('quill-editor', quillEditor)
+
     // 全局祖册
   }
 }
--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -12,9 +12,9 @@
               <el-input v-model="formData.title" style="width:60%"></el-input>
           </el-form-item>
           <el-form-item prop="content" label="内容">
-              <el-input v-model="formData.content" type="textarea" :rows="4"> </el-input>
+              <quill-editor v-model="formData.content" style="height:300px"> </quill-editor>
           </el-form-item>
-          <el-form-item  prop="cover" label="封面">
+          <el-form-item style="margin-top:100px" prop="cover" label="封面">
               <el-radio-group v-model="formData.cover.type" >
                   <el-radio :label="1">单图</el-radio>
                   <el-radio :label="3">三途</el-radio>
