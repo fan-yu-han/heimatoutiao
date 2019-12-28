399934cc921a1b877c6e64cb063dee07a437917c
hanyufan <1041218477@qq.com>
2019-12-28 11:53:36 +0800

    头条PC-发布文章-封面功能组件实现

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,7 @@ import layoutAside from './home/layout-aside'
 import layoutHeader from './home/layout-header'
 import breadCrumb from './common/bread-crumb'
 import { quillEditor } from 'vue-quill-editor'// qull富文本编辑器
+import CoverImage from './publish/cover-image'
 import 'quill/dist/quill.core.css'
 import 'quill/dist/quill.snow.css'
 import 'quill/dist/quill.bubble.css'
@@ -11,6 +12,7 @@ export default {
     Vue.component('layout-header', layoutHeader)// 注册一个全局主见
     Vue.component('bread-crumb', breadCrumb) // 全局注册面包屑组件
     Vue.component('quill-editor', quillEditor)
+    Vue.component('cover-image', CoverImage)
 
     // 全局祖册
   }
--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -15,14 +15,15 @@
               <quill-editor v-model="formData.content" style="height:300px"> </quill-editor>
           </el-form-item>
           <el-form-item style="margin-top:100px" prop="cover" label="封面">
-              <el-radio-group v-model="formData.cover.type" >
+              <el-radio-group @change='changeType' v-model="formData.cover.type" >
                   <el-radio :label="1">单图</el-radio>
                   <el-radio :label="3">三途</el-radio>
                   <el-radio :label="0">无图</el-radio>
                   <el-radio :label="-1">自动</el-radio>
               </el-radio-group>
-              {{formData.cover}}
           </el-form-item>
+          <!-- 封面组件 -->
+          <cover-image :list='formData.cover.images' ></cover-image>
           <el-form-item prop="channel_id" label="频道">
               <!-- {{channels}}   -->
               <el-select v-model="formData.channel_id">
@@ -80,8 +81,19 @@ export default {
           channel_id: null // 频道id
         }
       }
-    },
-    'formData.cover.type': function () {
+    }
+    // 'formData.cover.type': function () {
+    //   if (this.formData.cover.type === 0 || this.formData.cover.type === -1) {
+    //     this.formData.cover.images = []// 无图或者自动
+    //   } else if (this.formData.cover.type === 1) {
+    //     this.formData.cover.images = ['']// 单图
+    //   } else if (this.formData.cover.type === 3) {
+    //     this.formData.cover.images = ['', '', '']// 三图
+    //   }
+    // }
+  },
+  methods: {
+    changeType () {
       if (this.formData.cover.type === 0 || this.formData.cover.type === -1) {
         this.formData.cover.images = []// 无图或者自动
       } else if (this.formData.cover.type === 1) {
@@ -89,9 +101,7 @@ export default {
       } else if (this.formData.cover.type === 3) {
         this.formData.cover.images = ['', '', '']// 三图
       }
-    }
-  },
-  methods: {
+    },
     // 获取所有频道
     getChannels () {
       this.$axios({
