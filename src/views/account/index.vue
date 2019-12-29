c7f6365c34d7a53ac06407d3bd3f160decdbd661
hanyufan <1041218477@qq.com>
2019-12-29 22:08:55 +0800

    头条PC-账户信息-页面结构

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -1,9 +1,30 @@
 <template>
   <el-card>
-      <bread-crumb>
-      <template>账户信息</template>
-
+      <bread-crumb slot="header">
+        <template slot='title'>账户信息</template>
       </bread-crumb>
+      <!-- 表单 => 表单容器 -->
+      <el-form style="margin-left:100px" label-width="100px">
+          <el-form-item label="用户名">
+              <el-input style="width:40%"></el-input>
+          </el-form-item>
+           <el-form-item label="简介">
+               <el-input style="width:40%"></el-input>
+           </el-form-item>
+           <el-form-item label="邮箱">
+               <el-input style="width:40%"></el-input>
+           </el-form-item>
+           <el-form-item label="手机号">
+               <el-input disabled style="width:40%"></el-input>
+           </el-form-item>
+           <el-form-item>
+               <el-button type="primary">保存信息</el-button>
+           </el-form-item>
+      </el-form>
+      <!-- 上传组件 -->
+      <el-upload class='head-upload' action="" :show-file-list="false">
+          <img src="../../assets/img/back.jpg" alt="">
+      </el-upload>
   </el-card>
 </template>
 
@@ -13,6 +34,15 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang='less' scoped>
+.head-upload {
+   position: absolute;
+   right: 150px;
+   top:180px;
+    img {
+       width: 200px;
+       height: 200px;
+        border-radius: 50%;
+    }
+}
 </style>
