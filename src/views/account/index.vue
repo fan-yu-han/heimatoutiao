f9db882ebeb0961dcef598404e7e2125ed89139a
hanyufan <1041218477@qq.com>
2019-12-30 09:49:18 +0800

    头条PC-账户信息-用户数据保存

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -4,21 +4,21 @@
         <template slot='title'>账户信息</template>
       </bread-crumb>
       <!-- 表单 => 表单容器 -->
-      <el-form style="margin-left:100px" label-width="100px">
-          <el-form-item label="用户名">
+      <el-form ref="myForm" :model="formData" :rules="rules" style="margin-left:100px" label-width="100px">
+          <el-form-item prop="name" label="用户名">
               <el-input  v-model="formData.name" style="width:40%"></el-input>
           </el-form-item>
-           <el-form-item label="简介">
+           <el-form-item  label="简介">
                <el-input v-model="formData.intro" style="width:40%"></el-input>
            </el-form-item>
-           <el-form-item label="邮箱">
+           <el-form-item prop="email" label="邮箱">
                <el-input v-model="formData.email" style="width:40%"></el-input>
            </el-form-item>
-           <el-form-item label="手机号">
-               <el-input v-model="formData.mobile" disabled style="width:40%"></el-input>
+           <el-form-item  label="手机号">
+               <el-input  v-model="formData.mobile" disabled style="width:40%"></el-input>
            </el-form-item>
            <el-form-item>
-               <el-button type="primary">保存信息</el-button>
+               <el-button @click="saveUserIfo" type="primary">保存信息</el-button>
            </el-form-item>
       </el-form>
       <!-- 上传组件 -->
@@ -39,7 +39,16 @@ export default {
         email: '', // 邮箱
         mobile: ''
       },
-      defaultImg: require('../../assets/img/back.jpg')
+      defaultImg: require('../../assets/img/back.jpg'),
+      rules: {
+        name: [{ require: true, message: '用户名不能为空' }, {
+          min: 1, max: 7, message: '用户名长度在1-7个字符'
+        }],
+        email: [{ require: true, message: '邮箱不能为空' }, {
+          pattern: '^[A-Za-z0-9\u4e00-\u9fa5]+@[a-zA-Z0-9_-]+(.[a-zA-Z0-9_-]+)+$',
+          message: '邮箱格式不正确'
+        }]
+      }
     }
   },
   methods: {
@@ -49,6 +58,22 @@ export default {
         url: '/user/profile'
       }).then(result => {
         this.formData = result.data
+      })
+    },
+
+    saveUserIfo () {
+      this.$refs.myForm.validate().then(result => {
+        // 调用保存接口
+        this.$axios({
+          url: '/user/profile',
+          method: 'patch',
+          data: this.formData
+        }).then(result => {
+          this.$message({
+            type: 'success',
+            message: '保存用户信息  '
+          })
+        })
       })
     }
   },
