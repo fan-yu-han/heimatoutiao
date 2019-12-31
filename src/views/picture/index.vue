fe70ee5df9320b405a7f77bb4feac9ebb4eaef71
hanyufan <1041218477@qq.com>
2019-12-31 11:50:52 +0800

    PC-echarts在项目中的应用

--- src/views/picture/index.vue
+++ src/views/picture/index.vue
@@ -1,7 +1,7 @@
 <template>
   <el-card>
       <bread-crumb slot="header">
-            <template  slot="title">图文数据</template>
+            <template  slot="title">我的图文数据</template>
       </bread-crumb>
 
        <div ref="myChart" class='echarts'></div>
@@ -14,7 +14,90 @@ import echarts from 'echarts'
 export default {
   mounted () {
     let myChart = echarts.init(this.$refs.myChart)// 完成图表初始化
-    myChart.setOption()
+    myChart.setOption({
+      title: {
+        text: '堆叠区域图'
+      },
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+          type: 'cross',
+          label: {
+            backgroundColor: '#6a7985'
+          }
+        }
+      },
+      legend: {
+        data: ['邮件营销', '联盟广告', '视频广告', '直接访问', '搜索引擎']
+      },
+      toolbox: {
+        feature: {
+          saveAsImage: {}
+        }
+      },
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+      },
+      xAxis: [
+        {
+          type: 'category',
+          boundaryGap: false,
+          data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+        }
+      ],
+      yAxis: [
+        {
+          type: 'value'
+        }
+      ],
+      series: [
+        {
+          name: '邮件营销',
+          type: 'line',
+          stack: '总量',
+          areaStyle: {},
+          data: [120, 132, 101, 134, 90, 230, 210]
+        },
+        {
+          name: '联盟广告',
+          type: 'line',
+          stack: '总量',
+          areaStyle: {},
+          data: [220, 182, 191, 234, 290, 330, 310]
+        },
+        {
+          name: '视频广告',
+          type: 'line',
+          stack: '总量',
+          areaStyle: {},
+          data: [150, 232, 201, 154, 190, 330, 410]
+        },
+        {
+          name: '直接访问',
+          type: 'line',
+          stack: '总量',
+          areaStyle: { normal: {} },
+          data: [320, 332, 301, 334, 390, 330, 320]
+        },
+        {
+          name: '搜索引擎',
+          type: 'line',
+          stack: '总量',
+          label: {
+            normal: {
+              show: true,
+              position: 'top'
+            }
+          },
+          areaStyle: { normal: {} },
+          data: [820, 932, 901, 934, 1290, 1330, 1320]
+        }
+      ]
+    }
+    )
   }
 }
 </script>
